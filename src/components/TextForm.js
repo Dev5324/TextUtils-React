3c9d74d277a34ea4665fc9950f224e32c77ad069
Dev Panchal <devpanchal018@gmail.com>
2025-05-06 17:58:13 -0800

    TextUtils-React

---
+++ src/components/TextForm.js
@@ -0,0 +1,53 @@
+import React, { useState } from "react";
+
+export default function TextForm(props) {
+  const [text, setText] = useState("");
+  const handleUpClick = () => {
+    let newText = text.toUpperCase();
+    setText(newText);
+    props.showAlert("Converted to uppercase", "Success");
+  };
+  const handleloClick = () => {
+    let newText = text.toLowerCase();
+    setText(newText);
+  };
+
+  const handleOnChange = (event) => {
+    // console.log("On Change");
+    setText(event.target.value);
+  };
+  return (
+    <>
+      <div className="container">
+        <h1>{props.heading}</h1>
+        <div className="mb-3">
+          <textarea
+            className="form-control"
+            value={text}
+            onChange={handleOnChange}
+            style={{
+              backgroundColor: props.mode === "dark" ? "grey" : "white",
+            }}
+            id="myBox"
+            rows="8"
+          ></textarea>
+        </div>
+        <button className="btn btn-primary mx-1" onClick={handleUpClick}>
+          Convert to uppercase
+        </button>
+        <button className="btn btn-primary mx-1" onClick={handleloClick}>
+          Convert to uppercase
+        </button>
+      </div>
+      <div className="container my-2">
+        <h2>Your text summary</h2>
+        <p>
+          {text.split(" ").length} words and {text.length} characters
+        </p>
+        <p>{0.008 * text.split(" ").length} Minutes Read</p>
+        <h3>Preview</h3>
+        <p>{text}</p>
+      </div>
+    </>
+  );
+}
